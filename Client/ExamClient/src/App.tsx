4c998db19b8123d77469f752e4e109891e61006b
Nacho P. <ignaciopt9@gmail.com>
2024-11-19 19:46:53 +0100

    Front Update

--- Client/ExamClient/src/App.tsx
+++ Client/ExamClient/src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import PlayerForm from "./components/PlayerForm.tsx";
-import GameOverview from "./components/GameOverview.tsx/index.ts";
-import Balance from "./components/Balance.tsx/index.ts";
-import AdminDashboard from "./components/AdminDashboard.tsx/index.ts";
+import GameOverview from "./components/GameOverview.tsx";
+import Balance from "./components/Balance.tsx";
+import AdminDashboard from "./components/AdminDashboard.tsx";
 
 const App = () => {
   return (
