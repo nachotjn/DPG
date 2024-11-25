e5ada93f5eacaaa66fd6939455e8ceccea231f77
N. P. <ignaciopt9@gmail.com>
2024-11-25 01:17:36 +0100

    Updates Front-End
    Added new components, views and CSS from the views added regarding the prototype design.
    New fonts added.
    New node_modules @fortawesome

--- Client/ExamClient/src/App.tsx
+++ Client/ExamClient/src/App.tsx
@@ -1,25 +1,18 @@
-
-import './App.css';
-import LoginView from "./views/LoginView.tsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AdminView from "./views/AdminView.tsx";
-import BoardPLayersView from "./views/BoardPLayersView.tsx";
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LogInView from './views/LoginView';
+import PlayersGameView from './views/PlayersGameView';
+import PlayersHomeView from './views/PlayersHomeView'
 
 const App = () => {
   return (
-    <div className="App">
-    
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginView />} />
-          <Route path="/login" element={<LoginView />} />
-          <Route path="/admin" element={<AdminView />} />
-          <Route path="/playersboard" element={<BoardPLayersView />} />
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LogInView />} />
+        <Route path="/game" element={<PlayersGameView />} />
+        <Route path="/home" element={<PlayersHomeView />} />
 
-        </Routes>
-      </BrowserRouter>
-    </div>
+      </Routes>
+    </Router>
   );
 };
 
--- Client/ExamClient/src/views/LoginView.tsx
+++ Client/ExamClient/src/views/LoginView.tsx
@@ -1,37 +1,52 @@
-import UsernameInputField from "../components/UsernameInputField";
-import PasswordInputField from "../components/PasswordInputField";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faLock } from "@fortawesome/free-solid-svg-icons";
+import "./LogInView.css";
 
-export default function LogIn() {
+const LogInView = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    console.log('Username:', username);
+    console.log('Password:', password);
+  };
+
   return (
-    <section
-      style={{
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-      }}
-    >
-      <div
-        style={{
-          height: "60vh",
-          display: "flex",
-          justifyContent: "space-evenly",
-          alignItems: "center",
-          flexDirection: "column",
-        }}
-      >
-        <img
-          src="../src/assets/images/logo.png"
-          alt="Logo Image"
-          style={{ width: "200px", height: "200px" }}
-        />
-        <UsernameInputField />
-        <PasswordInputField />
-
-        <button>Log in</button>
+    <div className="login-container">
+      <div className="logo-container">
+        <img src="./src/assets/images/logo.png" alt="Logo" className="logo" /> 
       </div>
-    </section>
+      <h2 className="login-title">Welcome to Jerne IF Esbjerg</h2>
+      <div className="input-container">
+        <label className="input-label">Username</label>
+        <div className="input-with-icon">
+          <FontAwesomeIcon icon={faUser} className="icon" />
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Enter your username"
+            className="input-field"
+          />
+        </div>
+      </div>
+      <div className="input-container">
+        <label className="input-label">Password</label>
+        <div className="input-with-icon">
+          <FontAwesomeIcon icon={faLock} className="icon" />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter your password"
+            className="input-field"
+          />
+        </div>
+      </div>
+      <button className="login-button" onClick={handleLogin}>Log In</button>
+    </div>
   );
-}
+};
+
+export default LogInView;
