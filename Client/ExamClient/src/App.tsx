34362e3f7ddfb5781168732d62a1efe8942d86e6
Nacho P. <ignaciopt9@gmail.com>
2024-12-17 19:09:49 +0100

    Some changes

--- Client/ExamClient/src/App.tsx
+++ Client/ExamClient/src/App.tsx
@@ -1,18 +1,20 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LogInView from './pages/LoginView';
-import AdminHomeView from './pages/admin-views/home/AdminHomeView';
-import AdminGameView from './pages/admin-views/game/AdminGameView';
-import AdminMembersView from './pages/admin-views/members/AdminMembersView';
-import AdminHistoryView from './pages/admin-views/history/AdminHistoryView';
-import AdminWinnersView from './pages/admin-views/winners/AdminWinnersView';
-import PlayerGameView from './pages/player-views/game/PlayerGameView';
-import PlayerHomeView from './pages/player-views/home/PlayerHomeView';
-import PlayerProfileView from './pages/player-views/profile/PlayerProfileView';
-import PlayerHistoryView from './pages/player-views/history/PlayerHistoryView';
-import PlayerList from './components/Player/PlayerList'; 
-import PlayerForm from './components/Player/PlayerForm';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LogInView from "./pages/LoginView";
+import AdminHomeView from "./pages/admin-views/home/AdminHomeView";
+import AdminGameView from "./pages/admin-views/game/AdminGameView";
+import AdminMembersView from "./pages/admin-views/members/AdminMembersView";
+import AdminHistoryView from "./pages/admin-views/history/AdminHistoryView";
+import AdminWinnersView from "./pages/admin-views/winners/AdminWinnersView";
+import PlayerGameView from "./pages/player-views/game/PlayerGameView";
+import PlayerHomeView from "./pages/player-views/home/PlayerHomeView";
+import PlayerProfileView from "./pages/player-views/profile/PlayerProfileView";
+import PlayerHistoryView from "./pages/player-views/history/PlayerHistoryView";
+import PlayerList from "./components/Player/PlayerList";
+import PlayerForm from "./components/Player/PlayerForm";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// Test comment bla bla
 
 const App = () => {
   return (
@@ -29,11 +31,13 @@ const App = () => {
         <Route path="/player-game" element={<PlayerGameView />} />
         <Route path="/player-profile" element={<PlayerProfileView />} />
         <Route path="/player-history" element={<PlayerHistoryView />} />
-        
         {/* Nuevas rutas para gestionar jugadores */}
-        <Route path="/players" element={<PlayerList />} /> {/* Listado de jugadores */}
-        <Route path="/players/add" element={<PlayerForm />} /> {/* Agregar un nuevo jugador */}
-        <Route path="/players/edit/:id" element={<PlayerForm />} /> {/* Editar un jugador */}
+        <Route path="/players" element={<PlayerList />} />{" "}
+        {/* Listado de jugadores */}
+        <Route path="/players/add" element={<PlayerForm />} />{" "}
+        {/* Agregar un nuevo jugador */}
+        <Route path="/players/edit/:id" element={<PlayerForm />} />{" "}
+        {/* Editar un jugador */}
       </Routes>
     </Router>
   );
